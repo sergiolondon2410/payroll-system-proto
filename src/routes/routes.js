0fcd06e4a21bcdb2441af5ab4c694470d6cddc5a
sergiolondon2410 <sergiolondon2410@gmail.com>
2019-08-04 22:50:07 -0300

    manual user code insert

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const User = require('../models/User');
 
+// Get user list
 router.get('/', (req, res) => 
     User.findAll()
     .then(users => {
@@ -12,4 +13,29 @@ router.get('/', (req, res) =>
     .catch(err => {console.log('Error: ', err)})
 );
 
+//Add a user
+router.get('/add', (req, res) => {
+    const data = {
+        name: 'Elon',
+        last_name: 'Musk',
+        document: '1045998123',
+        salary: 827400,
+        account_id: 1,
+        email: 'elon.musk@mail.com'
+    };
+
+    let { name, last_name, document, salary, account_id, email } = data;
+    
+    //insert into table
+    User.create({
+        name,
+        last_name,
+        document,
+        salary,
+        account_id,
+        email
+    })
+    .then(user => res.redirect('/'))
+    .catch(err => console.log(err));
+});
 module.exports = router;
